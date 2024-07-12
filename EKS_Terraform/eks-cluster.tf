cbe3d7d662661f98cf178fd8131f48619d6f3ef9
adetit <dayotitiloye1@gmail.com>
2024-07-12 12:18:13 -0400

    Update eks-cluster.tf

--- EKS_Terraform/eks-cluster.tf
+++ EKS_Terraform/eks-cluster.tf
@@ -29,7 +29,7 @@ data "aws_vpc" "default" {
 }
 #cluster provision
 resource "aws_eks_cluster" "example" {
-  name     = "EKS_CLOUD_2"
+  name     = "EKS_CLOUD"
   role_arn = aws_iam_role.example.arn
 
 
